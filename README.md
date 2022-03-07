0d2409775414780b07a8a25309604b6f8722149b
0421cjy <cherryofsnow@gmail.com>
2022-03-08 01:03:04 +0900

    card list shuffle

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # tichu
 tichu card game
+
+## tichu phase
+1. large tichu phase (8 card)
+2. card exchange phase (+ 6 card and excahnge 1 card to each player)
+3. score phase
+
+## card 
+green, blue, red, black
+2,3,4,5,6,7,8,9,10,J,Q,K
+
+## special card
+1. bird
+2. phoenix
+3. dragon
+4. dog
+
+## card combination
+1. single
+2. pair
+3. straight pair
+4. triple
+5. full-house
+6. straight
+
+## card special combination
+1. straight flush
+2. four card
+
+## card score 
+1. 5 -> 5 * 4 = 20
+2. 10 -> 10 * 4 = 40
+3. K -> 10 * 4 = 40
+4. dragon -> 25 * 1 = 25
+5. phoenix -> -25 * 1 = -25
+6. (each round) Total score = 100
+
+## additional score
+perfact game success = 100
+small tichu success = 100
+large tichu success = 200
+small tichu failure = -100
+large tichu failure = -200
--- main.py
+++ main.py
@@ -1,5 +1,82 @@
+import random
+
+class Player:
+    hand_list = []
+    tomb_list = []
+    
+    def __init__(self, name):
+        self.name = name
+        
+    def drawCard(self, newCard):
+        self.hand_list.append(newCard)
+        
+    def winCard(self, winCard):
+        self.tomb_list.append(winCard)
+        
+class Card:
+    def __init__(self, symbol, number):
+        self.symbol = symbol
+        self.number = number
+        
+    def showMe(self):
+        return (self.symbol, self.number)
+    
+class SpecialCard:
+    symbol = 'special'
+    
+    def __init__(self, number):
+        self.number = number
+        
+    def showMe(self):
+        return (self.symbol, self.number)
+        
+class Deck:
+    def __init__(self, total_list):
+        self.deck_list = total_list
+        
+    def showTotalCardLen(self):
+        return len(self.deck_list)
+    
+    def showAllCard(self):
+        for i in self.deck_list:
+            print(i.showMe())
+            
+    def shuffleAllCard(self):
+        random.shuffle(self.deck_list)
+            
+def init_deck():
+    card_list = []
+    color_list = ['green', 'red', 'blue', 'black']
+        
+    for color in color_list:
+        for i in range(2, 11):
+            card_list.append(Card(color, i))
+        
+        card_list.append(Card(color, 'J'))
+        card_list.append(Card(color, 'Q'))
+        card_list.append(Card(color, 'K'))
+        
+    special_list = ['bird', 'pheonix', 'dragon', 'dog']
+    
+    for symbol in special_list:
+        card_list.append(SpecialCard(symbol))
+        
+    return sorted(card_list, key=lambda card: card.symbol)
+
 def main():
-    print ("main Function")
+    
+    list = init_deck()
+    
+    deck = Deck(list)
+    
+    print('\n before')
+    deck.showAllCard()
+    deck.shuffleAllCard()
+    
+    print('\n after')
+    deck.showAllCard()
+    
+    assert deck.showTotalCardLen() == 52, "deck count not 52"
     
 if __name__ == "__main__":
     main()
